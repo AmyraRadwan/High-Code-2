6e8e236bce49a0216cf929c1790caba54e5cc05e
Radwan, Amyra <amyra.radwan@haw-hamburg.de>
2022-11-24 20:35:30 +0100

    JS, CSS and Readme changed

--- README.md
+++ README.md
@@ -145,7 +145,7 @@ Wir bauen noch [obnoxious.css](https://tholman.com/obnoxious/) ein, eine CSS-Lib
 
 ## :fire: Step 2.1. – Einbinden der CSS-Library
 
-Um obnoxious.css einzubinden lade dir obnoxious.css [hier](https://github.com/tholman/obnoxious.css) herunter und speicher die Datei mir dem Namen unter `assets` in den `css` Ordner. Als nächstes bindest du die Datei in den `<head>` Tag deiner `index.html` Datei:
+Um obnoxious.css einzubinden lade dir obnoxious.css [hier](https://github.com/tholman/obnoxious.css) herunter und speicher die Datei mir dem Namen `obnoxious.css` unter `assets` in den `css` Ordner. Als nächstes bindest du die Datei in den `<head>` Tag deiner `index.html` Datei:
 
 ```
 <head>
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,13 @@
-const h1 = document.querySelector("h1");
+const h1 = document.querySelector("h1"); // const (Konstante) speichert einen Wert. Hier wird das h1 Element in die Konstante names "h1" gespeichert.
 
+// Eine Funktion die bei dem Event scroll etwas auf der Seite ausgeführt wird erstellt.
 window.addEventListener("scroll", function () {
-	const pixels = window.pageYOffset;
+	const pixels = window.pageYOffset; // In die const namens "pixels" wird der Wert der Y-Achse gespeichert.
 
-	const wght = 100 + pixels * 0.9;
-	const wdth = 100 + pixels * 0.9;
+	const wght = 100 + pixels * 0.9; // In die const namens "wght" wird die Rechnung 100 + der Wert der Y-Achse x 0.4 gespeichert.
+	const wdth = 100 + pixels * 0.9; // In die const namens "wdth" wird die Rechnung 100 + der Wert der Y-Achse x 0.1 gespeichert.
 
-	h1.style.fontVariationSettings = `"wght" ${wght}, "wdth" ${wdth}`;
+	h1.style.fontVariationSettings = `"wght" ${wght}, "wdth" ${wdth}`; // Für das h1 Element werden die Werte der CSS-Eigenschaft `font-variation-settings` (wght und wdth) den vorher definierten Konstanten, also den Berechnungen namens "wght" und "wdth" zugeteilt.
 });
+
+// Damit verändern sich die Werte der CSS-Eigenschaft `font-variation-settings` beim scrollen.
